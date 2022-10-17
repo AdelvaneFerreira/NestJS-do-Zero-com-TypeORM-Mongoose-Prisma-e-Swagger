c565b2fdd6a5291f6bc9e9e947ca1435e16c1629
Adelvane <adelvanefernandesfs@gmail.com>
2022-10-17 18:58:22 -0300

    Configurando o TypeORM com banco de dados Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,18 @@ import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [CoursesModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port:5432,
-    username: 'postgres',
-    password: 'docker',
-    autoLoadEntities: true,
-    synchronize: true,
-  })],
+  imports: [CoursesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'docker',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
